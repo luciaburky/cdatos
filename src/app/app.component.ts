63b5a27dc6fd3025645e06e039fb8568b6c18833
luciaburky <luciaburky1@gmail.com>
2025-08-30 10:45:44 -0300

    mostrar pagina en app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PaginaComponent } from './pagina/pagina.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [PaginaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
